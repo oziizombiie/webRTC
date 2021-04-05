168b079a1cd9cba32c34c37497956e38f02b7513
oziizombiie <oziizombiie@gmail.com>
2021-04-05 13:00:37 -0500

    disabled audio, camera, working on friendly interface

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 var apiKey;
 var sessionId;
 var token;
+var session;
+var subscriber;
+var publisher;
 
 function handleError(error) {
   if (error) {
@@ -10,32 +13,54 @@ function handleError(error) {
   }
 }
 
+function removeExtraVideos(){
+  let videoSubscribers = document.getElementsByClassName('OT_subscriber');
+  console.log(videoSubscribers.length);
+  if(videoSubscribers.length > 1){
+    let sus = document.getElementById('subscriber');
+    console.log(videoSubscribers);
+    let r = videoSubscribers[0];
+    console.log(r);
+    sus.appendChild(r);
+  }
+}
+
 function initializeSession() {
-  var session = OT.initSession(apiKey, sessionId);
+  session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
   session.on('streamCreated', function streamCreated(event) {
     var subscriberOptions = {
-      insertMode: 'append',
       width: '100%',
-      height: '100%'
+      height: '100%',
+      showControls: false
     };
-    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+    
+    subscriber =  session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
   });
 
   session.on('sessionDisconnected', function sessionDisconnected(event) {
     console.log('You were disconnected from the session.', event.reason);
   });
 
+  session.on("signal:handRaised", function(event) {
+    if(event.data.personHand != session.connection.connectionId){
+      alert(`${event.from.id} wants to speak`);
+    }
+    console.log("foo signal sent from connection " + event.from.id);
+    console.log("Signal data: " + event.data.personHand);
+  });
+  
+
+
   // initialize the publisher
   var publisherOptions = {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
+    width: '300px',
+    fitMode: 'cover',
+    showControls: false
   };
-  var publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+  publisher = OT.initPublisher('publisher', publisherOptions, handleError);
 
-  // Connect to the session
   session.connect(token, function callback(error) {
     if (error) {
       handleError(error);
@@ -45,6 +70,69 @@ function initializeSession() {
     }
   });
 }
+
+
+
+let btn_exit = document.getElementById('exit');
+let btn_video = document.getElementById('camera');
+let btn_hand = document.getElementById('hand');
+let btn_audio = document.getElementById('audio');
+// let btn_remove = document.getElementById('removeUser');
+var video_blocked = false;
+var audio_blocked = false;
+
+// btn_remove.addEventListener('click', function(){
+//   btn_remove.style.display = 'none';
+//   subscriber.destroy();
+// })
+
+btn_hand.addEventListener('click', function(){
+    session.signal({
+      type: "handRaised",
+      data: {personHand: session.connection.connectionId}
+    },
+    function(error) {
+      if (error) {
+        console.log("signal error: " + error.message);
+      } else {
+        console.log("signal sent");
+      }
+    }
+  );
+})
+
+
+btn_video.addEventListener('click', function(){
+  camera();
+});
+
+btn_audio.addEventListener('click', function(){
+  audio();
+});
+
+function audio(){
+  publisher.publishAudio(audio_blocked);
+  audio_blocked ? ( btn_audio.classList.remove('start') , btn_audio.classList.add('stop') ) : ( btn_audio.classList.add('start') , btn_audio.classList.remove('stop') )  ;
+  audio_blocked = !audio_blocked;
+}
+
+function camera(){
+  publisher.publishVideo(video_blocked);
+  video_blocked ? ( btn_video.classList.remove('start') , btn_video.classList.add('stop') ) : ( btn_video.classList.add('start') , btn_video.classList.remove('stop') )  ;
+  video_blocked = !video_blocked;
+}
+
+btn_exit.addEventListener('click', function() {
+    session.disconnect();
+    btn_exit.classList.remove('logout');
+    btn_exit.classList.add('login');
+    if(session.currentState == 'disconnected'){
+      btn_exit.classList.remove('login');
+      btn_exit.classList.add('logout');
+      initializeSession();
+    }
+});
+
 
 // See the config.js file.
 if (API_KEY && TOKEN && SESSION_ID) {
--- js/config.js
+++ js/config.js
@@ -10,6 +10,6 @@ var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '';
-var SESSION_ID = '';
-var TOKEN = '';
+var API_KEY = '45828062';
+var SESSION_ID = '1_MX40NTgyODA2Mn5-MTYxNjUwNjE3MDAzOH42aVV3YXhSc3JDbXhSdjFxaURCVElsTWV-UH4';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NTgyODA2MiZzaWc9ZDQzOTZjODAwNGYwYzU3ZTM2MThkYWFiOWQyNjI5YjVhMjNiYjVlYjpzZXNzaW9uX2lkPTFfTVg0ME5UZ3lPREEyTW41LU1UWXhOalV3TmpFM01EQXpPSDQyYVZWM1lYaFNjM0pEYlhoU2RqRnhhVVJDVkVsc1RXVi1VSDQmY3JlYXRlX3RpbWU9MTYxNjUwODc1MCZub25jZT0wLjQ0ODk1NzkxMzIyODU2OTcmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTYxNjU5NTE1MA==';
